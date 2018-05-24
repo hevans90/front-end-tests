be1f8f6cf80eda180fbd09706e6a6d7e9e80bb22
robt1019 <roberttaylor@tengroup.com>
2018-05-24 12:20:42 +0100

    feat(tdd): add starting point for tdd tests

--- src/app/fizz-buzz/fizz-buzz.service.spec.ts
+++ src/app/fizz-buzz/fizz-buzz.service.spec.ts
@@ -18,4 +18,48 @@ describe('FizzBuzzService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('fizzBuzz', () => {
+    it('should return Fizz if passed a number divisible by 3', () => {
+      expect(service.fizzBuzz(3)).toEqual('Fizz');
+      expect(service.fizzBuzz(6)).toEqual('Fizz');
+      expect(service.fizzBuzz(9)).toEqual('Fizz');
+      expect(service.fizzBuzz(18)).toEqual('Fizz');
+    });
+    it('should return Buzz if passed a number divisible by 5', () => {
+      expect(service.fizzBuzz(5)).toEqual('Buzz');
+      expect(service.fizzBuzz(10)).toEqual('Buzz');
+      expect(service.fizzBuzz(20)).toEqual('Buzz');
+      expect(service.fizzBuzz(25)).toEqual('Buzz');
+    });
+    it('should return FizzBuzz if passed a number divisible by 3 and 5', () => {
+      expect(service.fizzBuzz(15)).toEqual('FizzBuzz');
+      expect(service.fizzBuzz(30)).toEqual('FizzBuzz');
+      expect(service.fizzBuzz(45)).toEqual('FizzBuzz');
+      expect(service.fizzBuzz(60)).toEqual('FizzBuzz');
+    });
+    it('should return a number if passed a number', () => {
+      expect(service.fizzBuzz(1)).toEqual(1);
+      expect(service.fizzBuzz(2)).toEqual(2);
+      expect(service.fizzBuzz(13)).toEqual(13);
+    });
+  });
+
+  describe('run', () => {
+    it('should print 100 values', () => {
+      const spy = spyOn(console, 'log');
+      service.run();
+      expect(spy.calls.allArgs().length).toEqual(100);
+    });
+    it('should print the number 1 first', () => {
+      const spy = spyOn(console, 'log');
+      service.run();
+      expect(spy.calls.allArgs()[0][0]).toEqual(1);
+    });
+    it('should print the word Buzz last', () => {
+      const spy = spyOn(console, 'log');
+      service.run();
+      expect(spy.calls.allArgs()[99][0]).toEqual('Buzz');
+    });
+  });
 });
--- src/app/fizz-buzz/fizz-buzz.service.ts
+++ src/app/fizz-buzz/fizz-buzz.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FizzBuzzService {
 
-  constructor() { }
+  public run() {
+    for (let i = 1; i <= 100; i++) {
+      console.log(this.fizzBuzz(i));
+    }
+  }
+
+  public fizzBuzz(number: number): number | string {
+    if (number % 3 === 0 && number % 5 === 0) {
+      return 'FizzBuzz';
+    }
+    if (number % 3 === 0) {
+      return 'Fizz';
+    }
+    if (number % 5 === 0) {
+      return 'Buzz';
+    }
+    return number;
+  }
 }
